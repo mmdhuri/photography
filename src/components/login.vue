c78118a16c642f6a0fcb614e331b58965d4c6e2c
Manish Dhuri <manishdhuri2000@gmail.com>
2024-08-10 15:59:01 +0530

    Aded food categories Apis

---
+++ src/components/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input filled v-model="email" label="Enter Email *" hint="Name and surname" lazy-rules
+        :rules="[val => val && val.length > 0 || 'Please type something']" />
+      <q-input filled v-model="email" label="Enter Email *" hint="password" lazy-rules
+        :rules="[val => val && val.length > 0 || 'Please type something']" />
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { api } from 'boot/axios';
+
+const model = ref({
+  email: "",
+  password: ""
+});
+
+const onSubmit = async () => {
+  if (await v$.value.$validate()) {
+    processing.value = true;
+
+  }
+};
+
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,13 +7,40 @@
       <q-carousel-slide :name="4" img-src="~src/assets/Site Images/Slider-4.jpg" />
     </q-carousel>
   </div>
+  <div class="q-pa-md row items-start q-gutter-md justify-center q-mt-md" v-if="foodCategories">
+    <div class="col-md-3" v-for="foodCategories in foodCategories" :key="foodCategories.id">
+      <q-card class="my-card">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+        <q-card-section>
+          <div class="text-h6">{{ foodCategories.category }}</div>
+          <div class="text-subtitle2">{{ foodCategories.description }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref, onMounted } from 'vue';
+import { api } from 'boot/axios';
 
 const slide = ref(1);
+const foodCategories = ref(null);
+const error = ref(null);
+
+const getFoodCategories = async () => {
+  try {
+    const response = await api.get('/food-categories'); // Ensure $api is used
+    foodCategories.value = response.data;
+    console.log(response.data);
+  } catch (err) {
+    error.value = `Error: ${err.message}`;
+  }
+};
+
+onMounted(() => {
+  getFoodCategories();
+});
 defineOptions({
   name: 'IndexPage'
 });
